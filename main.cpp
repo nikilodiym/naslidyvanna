aca01126204d0ab93ec80d2b1948d4641946ce95
Nikita Krenov <nikitabaram@gmail.com>
2024-09-28 15:55:48 +0300

    fixed code

--- main.cpp
+++ main.cpp
@@ -3,7 +3,22 @@
 
 using namespace std;
 
+class A {
+public:
+    void show() {
+        cout << "Class A method" << endl;
+    }
+};
+
+class B : virtual public A {};
+
+class C : virtual public A {};
+
+class D : public B, public C {};
+
 int main() {
+    D obj;
+    obj.show();
 
 	system("pause");
 	return 0;
